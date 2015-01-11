6b44c9d2c40b9335c976430e6a66f6473770be8e
Gregor Ottmann <zaphod@bluephod.net>
2015-01-11 14:49:18 +0100

    Supported more of the FatLight commands

--- app/src/main/java/net/bluephod/fatlightcontrol/ControlActivity.java
+++ app/src/main/java/net/bluephod/fatlightcontrol/ControlActivity.java
@@ -3,25 +3,29 @@ package net.bluephod.fatlightcontrol;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.common.base.Splitter;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This activity does everything.
- *
+ * <p/>
  * Inspired by https://bellcode.wordpress.com/2012/01/02/android-and-arduino-bluetooth-communication/
  */
 public class ControlActivity extends ActionBarActivity {
@@ -32,15 +36,24 @@ public class ControlActivity extends ActionBarActivity {
 	private Button onButton;
 	private Button offButton;
 	private Spinner modeSpinner;
+	private EditText trgbEditText;
+	private Button trgbButton;
+	private EditText delayEditText;
+	private Button delayButton;
+
 	private BluetoothDevice device;
 	private BluetoothSocket socket;
 	private OutputStreamWriter out;
 	private InputStream in;
 	private boolean stopWorker = false;
 
+	private String lastCommand = "";
+	private Map<String, String> status;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		status = new HashMap<String, String>();
 		setContentView(R.layout.activity_control);
 
 		connectionTextView = (TextView) findViewById(R.id.connectionTextView);
@@ -48,6 +61,12 @@ public class ControlActivity extends ActionBarActivity {
 		onButton = (Button) findViewById(R.id.onButton);
 		offButton = (Button) findViewById(R.id.offButton);
 		modeSpinner = (Spinner) findViewById(R.id.modeSpinner);
+		trgbEditText = (EditText) findViewById(R.id.trgbEditText);
+		trgbButton = (Button) findViewById(R.id.trgbButton);
+		delayEditText = (EditText) findViewById(R.id.delayEditText);
+		delayButton = (Button) findViewById(R.id.delayButton);
+
+		setInputsEnabled(false);
 
 		connectionTextView.setText(R.string.err_fatlight_not_connected);
 		resultTextView.setText("");
@@ -58,15 +77,24 @@ public class ControlActivity extends ActionBarActivity {
 				setActive(true);
 			}
 		});
-		onButton.setEnabled(false);
-
 		offButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				setActive(false);
 			}
 		});
-		offButton.setEnabled(false);
+		trgbButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				setTrgb(trgbEditText.getText().toString());
+			}
+		});
+		delayButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				setDelay(delayEditText.getText().toString());
+			}
+		});
 
 		ArrayAdapter<String> modeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
 		modeAdapter.add(getString(R.string.control_mode_direct));
@@ -87,30 +115,36 @@ public class ControlActivity extends ActionBarActivity {
 
 			}
 		});
-		modeSpinner.setEnabled(true);
 	}
 
 	private void setInputsEnabled(boolean enabled) {
 		onButton.setEnabled(enabled);
 		offButton.setEnabled(enabled);
 		modeSpinner.setEnabled(enabled);
+		trgbButton.setEnabled(enabled);
+		trgbEditText.setEnabled(enabled);
+		delayButton.setEnabled(enabled);
+		delayEditText.setEnabled(enabled);
 	}
 
 	private void sendCommand(String command) {
-		setInputsEnabled(false);
-		resultTextView.setText("");
+		if (socket.isConnected()) {
+			setInputsEnabled(false);
+			resultTextView.setText("");
 
-		try {
-			out.write(command + "\r\n");
-			out.flush();
-		} catch (IOException e) {
-			Log.e(TAG, "Error writing command", e);
-			setInputsEnabled(true);
+			try {
+				out.write(command + "\r\n");
+				out.flush();
+			} catch (IOException e) {
+				Log.e(TAG, "Error writing command", e);
+				setInputsEnabled(true);
+			}
+		} else {
+			Log.e(TAG, "Command can't be written to unconnected socket.");
 		}
 	}
 
-	private void connect(String name, String address) throws IOException
-	{
+	private void connect(String name, String address) throws IOException {
 		device = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(address);
 		socket = device.createRfcommSocketToServiceRecord(Constants.UUID_SPP);
 		socket.connect();
@@ -119,8 +153,9 @@ public class ControlActivity extends ActionBarActivity {
 
 		receiveData();
 
+		getStatus();
+
 		connectionTextView.setText(name + " " + address);
-		setInputsEnabled(true);
 	}
 
 	private void disconnect() throws IOException {
@@ -138,49 +173,36 @@ public class ControlActivity extends ActionBarActivity {
 		final byte delimiter = 10; //This is the ASCII code for a newline character
 
 		stopWorker = false;
-		Thread workerThread = new Thread(new Runnable()
-		{
-			public void run()
-			{
+		Thread workerThread = new Thread(new Runnable() {
+			public void run() {
 				int readBufferPosition = 0;
 				byte[] readBuffer = new byte[1024];
 
-				while(!Thread.currentThread().isInterrupted() && !stopWorker)
-				{
-					try
-					{
+				while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+					try {
 						int bytesAvailable = in.available();
-						if(bytesAvailable > 0)
-						{
+						if (bytesAvailable > 0) {
 							byte[] packetBytes = new byte[bytesAvailable];
 							bytesAvailable = in.read(packetBytes);
-							for(int i=0;i<bytesAvailable;i++)
-							{
+							for (int i = 0; i < bytesAvailable; i++) {
 								byte b = packetBytes[i];
-								if(b == delimiter)
-								{
+								if (b == delimiter) {
 									byte[] encodedBytes = new byte[readBufferPosition];
 									System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
 									final String data = new String(encodedBytes, "US-ASCII");
 									readBufferPosition = 0;
 
-									handler.post(new Runnable()
-									{
-										public void run()
-										{
+									handler.post(new Runnable() {
+										public void run() {
 											handleResult(data);
 										}
 									});
-								}
-								else
-								{
+								} else {
 									readBuffer[readBufferPosition++] = b;
 								}
 							}
 						}
-					}
-					catch (IOException ex)
-					{
+					} catch (IOException ex) {
 						stopWorker = true;
 					}
 				}
@@ -190,20 +212,46 @@ public class ControlActivity extends ActionBarActivity {
 		workerThread.start();
 	}
 
-
 	private void setMode(int mode) {
+		lastCommand = "MODE";
 		sendCommand("MODE " + mode);
 	}
 
 	private void setActive(boolean active) {
-		sendCommand(active ? "ON" : "OFF");
+		lastCommand = active ? "ON" : "OFF";
+		sendCommand(lastCommand);
+	}
+
+	private void setTrgb(String trgbString) {
+		lastCommand = "SET";
+		sendCommand(lastCommand + " " +trgbString);
+	}
+
+	private void setDelay(String delayString) {
+		lastCommand = "DELAY";
+		sendCommand(lastCommand + " " +delayString);
 	}
 
 	private void getStatus() {
-		sendCommand("STATUS");
+		lastCommand = "STATUS";
+		sendCommand(lastCommand);
+	}
+
+	private void handleStatus(String result) {
+		if(result.startsWith("YEAH")) {
+			status = Splitter.on(" ").trimResults().withKeyValueSeparator(":").split(result.substring(5));
+
+			trgbEditText.setText(status.get("TRGB"));
+			delayEditText.setText(status.get("DELAY"));
+			modeSpinner.setSelection(Integer.parseInt(status.get("MODE")));
+		}
 	}
 
 	private void handleResult(String result) {
+		if (lastCommand.equals("STATUS")) {
+			handleStatus(result);
+		}
+
 		resultTextView.setText(result);
 		setInputsEnabled(true);
 	}
@@ -220,8 +268,7 @@ public class ControlActivity extends ActionBarActivity {
 			} catch (IOException e) {
 				Log.e(TAG, "Exception on connect", e);
 			}
-		}
-		else {
+		} else {
 			connectionTextView.setText(R.string.err_fatlight_not_specified);
 		}
 	}
